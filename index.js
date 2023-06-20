a8e88cdb677275d06b759e66f24138165070cf14
Fishbite <43952420+Fishbite@users.noreply.github.com>
2023-06-20 19:57:56 +0100

    created conversation array, added user input
    Created `conversationArr` to hold everything sent to and from the ai
    we then pushed the user's input to the ai in an object

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
-import { Configuration, OpenAIApi } from 'openai'
-import { process } from './env'
+import { Configuration, OpenAIApi } from "openai";
+import { process } from "./env";
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-})
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAIApi(configuration);
 
-const chatbotConversation = document.getElementById('chatbot-conversation')
+const chatbotConversation = document.getElementById("chatbot-conversation");
 
-document.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const userInput = document.getElementById('user-input')
-    const newSpeechBubble = document.createElement('div')
-    newSpeechBubble.classList.add('speech', 'speech-human')
-    chatbotConversation.appendChild(newSpeechBubble)
-    newSpeechBubble.textContent = userInput.value
-    userInput.value = ''
-    chatbotConversation.scrollTop = chatbotConversation.scrollHeight
-})
+// A single source of truth. Everything we ask of the ai and everything
+// it responds with is stored in this array
+const conversationArr = [
+  {
+    role: "system", // this tells the ai that an instruction follows
+    // this is the instruction to the ai & not part of the conversation
+    content:
+      "You are a highly knowledgeable assistant that gives short concise answers",
+  },
+];
+
+document.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const userInput = document.getElementById("user-input");
+
+  // add an object holding the user's input to the conversation array
+  conversationArr.push({
+    role: "user",
+    content: userInput.value,
+  });
+
+  const newSpeechBubble = document.createElement("div");
+  newSpeechBubble.classList.add("speech", "speech-human");
+  chatbotConversation.appendChild(newSpeechBubble);
+  newSpeechBubble.textContent = userInput.value;
+  userInput.value = "";
+  chatbotConversation.scrollTop = chatbotConversation.scrollHeight;
+});
 
 function renderTypewriterText(text) {
-    const newSpeechBubble = document.createElement('div')
-    newSpeechBubble.classList.add('speech', 'speech-ai', 'blinking-cursor')
-    chatbotConversation.appendChild(newSpeechBubble)
-    let i = 0
-    const interval = setInterval(() => {
-        newSpeechBubble.textContent += text.slice(i-1, i)
-        if (text.length === i) {
-            clearInterval(interval)
-            newSpeechBubble.classList.remove('blinking-cursor')
-        }
-        i++
-        chatbotConversation.scrollTop = chatbotConversation.scrollHeight
-    }, 50)
+  const newSpeechBubble = document.createElement("div");
+  newSpeechBubble.classList.add("speech", "speech-ai", "blinking-cursor");
+  chatbotConversation.appendChild(newSpeechBubble);
+  let i = 0;
+  const interval = setInterval(() => {
+    newSpeechBubble.textContent += text.slice(i - 1, i);
+    if (text.length === i) {
+      clearInterval(interval);
+      newSpeechBubble.classList.remove("blinking-cursor");
+    }
+    i++;
+    chatbotConversation.scrollTop = chatbotConversation.scrollHeight;
+  }, 50);
 }
